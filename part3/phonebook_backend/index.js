8264f0b9de0da42cddf74b091b3ab90473fa4dce
LiamMahoney <liammahoney96@gmail.com>
2021-05-30 12:04:43 -0500

    exercise 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -29,10 +29,22 @@ app.get('/info', (request, response) => {
     const info = `Phonebook has info for ${persons.length} people\n\n${new Date()}`;
 
     response.end(info);
-})
+});
 
 app.get('/api/persons', (request, response) => {
     response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+
+    const person = persons.find(p => p.id === id);
+
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
 });
 
 const PORT = 3001;
