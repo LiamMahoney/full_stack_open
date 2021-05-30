9a319c60c767418ed8d4caeea24a33a6aa8d9495
LiamMahoney <liammahoney96@gmail.com>
2021-05-30 16:35:01 -0500

    exercise 3.9

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,8 @@ morgan.token('body', function getBody(req) {
 
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+app.use(cors());
 
 let persons = [
     { 
