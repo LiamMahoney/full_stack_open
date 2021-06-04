f933da3404371e4bceef0f932bdfa4e5f5615ab3
LiamMahoney <liammahoney96@gmail.com>
2021-06-03 21:56:03 -0500

    exercise 3.13

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 
@@ -14,28 +16,28 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static('build'))
 
-let persons = [
-    { 
-        "name": "Arto Hellas", 
-        "number": "040-123456",
-        "id": 1
-    },
-    { 
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523",
-        "id": 2
-    },
-    { 
-        "name": "Dan Abramov", 
-        "number": "12-43-234345",
-        "id": 3
-    },
-    { 
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122",
-        "id": 4
-    }
-]
+// let persons = [
+//     { 
+//         "name": "Arto Hellas", 
+//         "number": "040-123456",
+//         "id": 1
+//     },
+//     { 
+//         "name": "Ada Lovelace", 
+//         "number": "39-44-5323523",
+//         "id": 2
+//     },
+//     { 
+//         "name": "Dan Abramov", 
+//         "number": "12-43-234345",
+//         "id": 3
+//     },
+//     { 
+//         "name": "Mary Poppendieck", 
+//         "number": "39-23-6423122",
+//         "id": 4
+//     }
+// ]
 
 app.get('/info', (request, response) => {
     const info = `Phonebook has info for ${persons.length} people\n\n${new Date()}`;
@@ -44,7 +46,9 @@ app.get('/info', (request, response) => {
 });
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons);
+    Person.find({}).then((persons) => {
+        response.json(persons);
+    });
 });
 
 app.post('/api/persons', (request, response) => {
@@ -74,15 +78,16 @@ app.post('/api/persons', (request, response) => {
 });
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
+    Person.findById(request.params.id).then((person) => {
+        if (person) {
+            response.json(person);
+        } else {
+            response.status(404).end();
+        }
+    }).catch((err) => {
+        response.status(500).end();
+    })
 
-    const person = persons.find(p => p.id === id);
-
-    if (person) {
-        response.json(person);
-    } else {
-        response.status(404).end();
-    }
 });
 
 app.delete('/api/persons/:id', (request, response) => {
