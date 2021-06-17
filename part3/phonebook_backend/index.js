cc4455218e728ec7efd94318ec6291784d039c96
LiamMahoney <liammahoney96@gmail.com>
2021-06-16 21:39:58 -0500

    exercise 3.15

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -16,29 +16,6 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :b
 app.use(cors());
 app.use(express.static('build'))
 
-// let persons = [
-//     { 
-//         "name": "Arto Hellas", 
-//         "number": "040-123456",
-//         "id": 1
-//     },
-//     { 
-//         "name": "Ada Lovelace", 
-//         "number": "39-44-5323523",
-//         "id": 2
-//     },
-//     { 
-//         "name": "Dan Abramov", 
-//         "number": "12-43-234345",
-//         "id": 3
-//     },
-//     { 
-//         "name": "Mary Poppendieck", 
-//         "number": "39-23-6423122",
-//         "id": 4
-//     }
-// ]
-
 app.get('/info', (request, response) => {
     const info = `Phonebook has info for ${persons.length} people\n\n${new Date()}`;
 
@@ -84,11 +61,11 @@ app.get('/api/persons/:id', (request, response) => {
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id);
-
-    persons = persons.filter(p => p.id !== id);
-
-    response.status(204).end();
+    Person.findByIdAndRemove(request.params.id).then((perosn) => {
+        response.status(204).end();
+    }).catch((error) => {
+        response.status(403).end();
+    });
 })
 
 const PORT = process.env.PORT || 3001;
