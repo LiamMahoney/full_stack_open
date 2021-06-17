227ad340a2787633e051a3fa6576e7ff8d02b751
LiamMahoney <liammahoney96@gmail.com>
2021-06-16 21:35:55 -0500

    end of 3C

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -48,25 +48,58 @@ app.post('/api/notes/', (request, response) => {
     });
 });
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
     Note.findById(request.params.id).then((note) => {
-        response.json(note);
-    });
+        if (note) {
+            response.json(note);
+        } else {
+            response.status(404).end();
+        }
+    }).catch((error) => {
+        next(error);
+    })
 });
 
-app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id);
-    notes = notes.filter(note => note.id !== id);
-
-    response.status(204).end();
+app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndRemove(request.params.id).then((result) => {
+        response.status(204).end();
+    }).catch((error) => {
+        next(error);
+    })
 })
 
+app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body;
+
+    const note = {
+        content: body.content,
+        important: body.important
+    }
+
+    Note.findByIdAndUpdate(request.params.id, note, {new: true}).then((updatedNote) => {
+        response.json(updatedNote);
+    }).catch((error) => {
+        next(error);
+    })
+})
 
 function unknownEndpoint (request, response) {
     response.status(404).send({error: 'unkown endpoint'});
 }
 
 app.use(unknownEndpoint);
+
+function errorHandler(error, request, response, next) {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformed id'});
+    }
+
+    next(error);
+}
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
