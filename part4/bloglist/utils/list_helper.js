48c7c9cb98e2593c9d04dc75fc931d3f24e81567
LiamMahoney <liammahoney96@gmail.com>
2021-06-20 15:53:46 -0500

    exercise 4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ function dummy(blogs) {
     return 1;
 }
 
+function totalLikes(blogs) {
+    return blogs.reduce((accumulator, currentValue) => {
+        return accumulator += currentValue.likes
+    }, 0);
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 };
