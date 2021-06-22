c4a2d9c37af213a2f905ed65244e3fb9f50153ab
LiamMahoney <liammahoney96@gmail.com>
2021-06-21 19:53:10 -0500

    exercise 4.6

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -23,8 +23,43 @@ function favoriteBlog(blogs) {
     return favoriteBlog;
 }
 
+function mostBlogs(blogs) {
+    let authors = blogs.reduce((accumulator, currentValue) => {
+
+        let currAuthor = accumulator.find((author) => {
+            // console.log('currentValue', currentValue);
+            // console.log('author', author);
+            return author.author === currentValue.author;
+        });
+
+        if (currAuthor) {
+            currAuthor.blogs++;
+        } else {
+            accumulator.push({
+                author: currentValue.author,
+                blogs: 1
+            });
+        }
+
+        return accumulator;
+    }, []);
+
+    let maxAuthor = undefined;
+
+    authors.forEach((author) => {
+        if (maxAuthor === undefined) {
+            maxAuthor = author;
+        } else if (author.blogs > maxAuthor.blogs) {
+            maxAuthor = author;
+        }
+    });
+
+    return maxAuthor;
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 };
