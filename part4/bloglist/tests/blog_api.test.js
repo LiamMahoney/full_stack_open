4fae4aee699f96e4015f7e5652e797c8a0561627
LiamMahoney <liammahoney96@gmail.com>
2021-07-05 21:25:45 -0500

    exercise 4.8

---
+++ part4/bloglist/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const supertest = require('supertest');
+const app = require('../app');
+const Blog = require('../models/blog');
+const helper = require('./test_helper');
+
+const api = supertest(app);
+
+beforeEach(async () => {
+    await Blog.deleteMany({});
+
+    await Promise.all(
+        helper.initialBlogs.map(async (b) => {
+            let blog = new Blog(b);
+            return blog.save();
+        })
+    );
+});
+
+test('all blogs are returned as json', async () => {
+    const response = await api
+        .get('/api/blogs')
+        .expect(200)
+        .expect('Content-Type', /application\/json/);
+
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+});
+
+afterAll(() => {
+    mongoose.connection.close();
+})
