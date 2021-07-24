8f68279231cc6bfb5951f9bdf05054329b1afe19
LiamMahoney <liammahoney96@gmail.com>
2021-07-23 23:34:25 -0500

    exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,30 @@
-import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import React, { useState } from 'react'
+
+const Blog = ({blog}) => {
+  const [viewDetails, setViewDetails] = useState(false);
+
+  const detialStyle = {
+    borderStyle: 'solid',
+    padding: 2,
+    margin: 5
+  }
+
+  const buttonText = viewDetails ? 'hide' : 'view'
+  const detailsHiddenStyle = { display: viewDetails ? '' : 'none' }
+  
+  return (
+    <div style={detialStyle}>
+      {blog.title} {blog.author} 
+      <button onClick={() => setViewDetails(!viewDetails)}>{buttonText}</button>
+      <div style={detailsHiddenStyle}>
+        <p>{blog.url}</p>
+        <div>
+          likes {blog.likes} <button>like</button>
+        </div>
+        <p>{blog.user && 'name' in blog.user ? blog.user.name : 'unknown'}</p>
+      </div>
+    </div>  
+  )
+}
 
 export default Blog
